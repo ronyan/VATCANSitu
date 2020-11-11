2d7c972078d77ea7bd882f0b18b61118a27cf123
ronyan <r.yan163@gmail.com>
2020-11-11 11:02:14 -0400

    fixed small memory leak issue

--- CSiTRadar.cpp
+++ CSiTRadar.cpp
@@ -103,7 +103,7 @@ void CSiTRadar::OnRefresh(HDC hdc, int phase)
 		if ((time - oldTime) / CLOCKS_PER_SEC > 300) {
 			CAsync* data = new CAsync();
 			data->Plugin = GetPlugIn();
-			_beginthread(CDataHandler::GetVatsimAPIData, 0, (void*)&data);
+			_beginthread(CDataHandler::GetVatsimAPIData, 0, (void*) data);
 			oldTime = clock();
 		}
 	}
