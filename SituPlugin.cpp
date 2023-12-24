8cb3e227ed414f3b0a40633258ce35b5ca56a168
ronyan <r.yan163@gmail.com>
2023-12-24 00:40:25 -0500

    version update

--- SituPlugin.cpp
+++ SituPlugin.cpp
@@ -590,7 +590,7 @@ LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam) {
 SituPlugin::SituPlugin()
 	: EuroScopePlugIn::CPlugIn(EuroScopePlugIn::COMPATIBILITY_CODE,
 		"VATCANSitu",
-		"0.5.9.1",
+		"0.5.10.0",
 		"Ron Yan",
 		"Attribution-NonCommercial 4.0 International (CC BY-NC 4.0)")
 {
